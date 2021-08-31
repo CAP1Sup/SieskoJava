9a0292c8f47476f5fd88a1aed88b15c84ed505bc
cap1sup <xmanpiper@gmail.com>
2021-08-31 09:32:43 -0400

    Added summer info print exercise

---
+++ PrintSummerInfo/PrintSummerInfo.java
@@ -0,0 +1,14 @@
+// Christian Piper
+// 8/31/21
+
+// Main package declaration
+package PrintSummerInfo;
+
+// Class declaration
+class HelloWorld {
+    public static void main(String args[]) {
+
+        // Print out
+        System.out.println("\nHello! My summer was great! I went to the beach and to the lake!\n");
+    }
+}
