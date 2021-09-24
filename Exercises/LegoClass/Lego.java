2e426462bc09dcb831170e665d335dfb598c491d
cap1sup <xmanpiper@gmail.com>
2021-09-24 10:17:54 -0400

    Added Lego and Time class

---
+++ Exercises/LegoClass/Lego.java
@@ -0,0 +1,41 @@
+/**
+ * @author Christian Piper
+ * @date 9/24/21
+ */
+
+package Exercises.LegoClass;
+
+public class Lego {
+
+    // Instance variables
+    int studs, height;
+    String color, shape;
+
+    Lego() {
+        studs = 0;
+        height = 0;
+        color = "";
+        shape = "";
+    }
+
+    Lego(int ns, int h, String c, String s) {
+        studs = ns;
+        height = h;
+        color = c;
+        s = shape;
+    }
+
+    int getStuds() {return studs;}
+    int getHeight() {return height;}
+    String getColor() {return color;}
+    String getShape() {return shape;}
+
+    void setStuds(int s) {studs = s;}
+    void setHeight(int h) {height = h;}
+    void setColor(String c) {color = c;}
+    void setShape(String s) {shape = s;}
+
+    public String toString() {
+        return ("Studs: " + studs + " | Height: " + height + " | Color: " + color + " | Shape: " + shape);
+    }
+}
---
+++ Exercises/TimeClass/TimeClass.java
@@ -0,0 +1,79 @@
+/**
+ * @author Christian Piper
+ * @date 9/24/21
+ */
+
+package Exercises.TimeClass;
+
+public class TimeClass {
+    private int hrs, min, sec;
+
+    TimeClass() {
+        hrs = 0;
+        min = 0;
+        sec = 0;
+    }
+
+    TimeClass(int hours) {
+        hrs = hours;
+        min = 0;
+        sec = 0;
+    }
+
+    TimeClass(int hours, int mins) {
+        hrs = hours;
+        min = mins;
+        sec = 0;
+        fixTime();
+    }
+
+    TimeClass(int hours, int mins, int secs) {
+        hrs = hours;
+        min = mins;
+        sec = secs;
+        fixTime();
+    }
+
+    public int getHours() {
+        return hrs;
+    }
+
+    public void setHours(int hours) {
+        hrs = hours;
+    }
+
+    public int getMins() {
+        return min;
+    }
+
+    public void setMins(int mins) {
+        min = mins;
+        fixTime();
+    }
+
+    public int getSec() {
+        return sec;
+    }
+
+    public void setSec(int secs) {
+        sec = secs;
+        fixTime();
+    }
+
+    public void addTime(int hours, int minutes, int seconds) {
+        hrs = hours;
+        min = minutes;
+        sec = seconds;
+        fixTime();
+    }
+
+    private void fixTime() {
+        sec = sec % 60;
+        min = (min % 60) + (sec / 60);
+        hrs = hrs + (min / 60);
+    }
+
+    public String toString() {
+        return ("H: " + hrs + " | M: " + min + " | S: " + sec);
+    }
+}
