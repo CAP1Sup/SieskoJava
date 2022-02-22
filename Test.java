7ecb626b3f0ddf4b9d239485d83107c4c22c885a
cap1sup <xmanpiper@gmail.com>
2022-02-22 11:13:31 -0500

    Added function to print double bits

--- Test.java
+++ Test.java
@@ -1,14 +1,17 @@
 public class Test {
     public static void main(String[] args) {
-        Test object = new Test();
-
-        object.bbb("January", 4);
+        printBits(0.1);
     }
 
-    public void bbb(String s, int p) {
-        if (p >= 0) {
-            System.out.print(s.substring(p, p + 1));
-            bbb(s, p - 1);
-        }
+    public static void printBits(double num) {
+        String max = "0" + Long.toBinaryString(Double.doubleToLongBits(num));
+
+        String sign = "Sign: " + max.substring(0, 1);
+        String exponent = "Exponent: " + max.substring(1, 12); // 11111111110
+        String mantissa = "Mantissa: " + max.substring(12);
+
+        System.out.println(sign); // 0 - positive
+        System.out.println(exponent); // 2046 - 1023 = 1023
+        System.out.println(mantissa); // 0.99999...8
     }
 }
